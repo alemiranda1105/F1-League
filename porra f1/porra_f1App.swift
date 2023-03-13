6db65b192da01440684c301d81e6ce17c714bf1e
Alejandro Miranda <amiranda110500@gmail.com>
2023-03-13 20:17:45 +0000

    Initial Commit

---
+++ porra f1/porra_f1App.swift
@@ -0,0 +1,17 @@
+//
+//  porra_f1App.swift
+//  porra f1
+//
+//  Created by Alejandro Miranda on 13/3/23.
+//
+
+import SwiftUI
+
+@main
+struct porra_f1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
