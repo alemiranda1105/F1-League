6bfc159b4426e81d465dcfe760379c152d25df44
Alejandro Miranda <amiranda110500@gmail.com>
2023-06-06 20:24:29 +0100

    Update drivers view

--- porra f1/presentation/read-drivers/DriversListItemView.swift
+++ porra f1/presentation/read-drivers/DriversListItemView.swift
@@ -12,26 +12,30 @@ struct DriversListItemView: View {
     
     var body: some View {
         HStack (spacing: 30) {
-            VStack(alignment: .leading) {
-                Text(driver.permanentNumber)
-                    .font(.system(size: 32, weight: .heavy))
-                Text(driver.code)
-                    .font(.system(size: 24, weight: .bold))
-            }
-            
-            VStack(alignment: .leading) {
-                Text("\(driver.givenName) \(driver.familyName)")
-                    .font(.system(size: 24, weight: .bold))
-                    .lineLimit(2)
-                Text(driver.nationality)
-                    .font(.system(size: 20, weight: .light))
-            }
+            Image("DRIVER-\(driver.permanentNumber)")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity)
             
             Spacer()
             
-            Link(destination: URL(string: driver.url)!) {
-                Image(systemName: "network")
+            VStack(alignment: .center) {
+                Text(driver.permanentNumber)
+                    .font(.system(size: 32, weight: .heavy))
+                Text("\(driver.givenName) \(driver.familyName)")
+                    .font(.system(size: 24, weight: .bold))
+                    .lineLimit(2)
+                    .multilineTextAlignment(.center)
+                
+                Text(driver.nationality)
+                    .font(.system(size: 20, weight: .light))
+                
+                Link(destination: URL(string: driver.url)!) {
+                    Text("Wikipedia")
+                }
             }
+            .frame(maxWidth: .infinity)
+            
             
         }
         .padding()
@@ -45,7 +49,7 @@ struct DriversListItemView: View {
 
 struct DriversListItemView_Previews: PreviewProvider {
     static var previews: some View {
-        DriversListItemView(driver: .constant(Driver(code: "MIR", dateOfBirth: "2000/05/11", driverId: "miranda", familyName: "Miranda", givenName: "Alejandro", nationality: "Spanish", permanentNumber: "21", url: "https://github.com/alemiranda1105")))
+        DriversListItemView(driver: .constant(Driver(code: "MIR", dateOfBirth: "2000/05/11", driverId: "miranda", familyName: "Miranda", givenName: "Alejandro", nationality: "Spanish", permanentNumber: "33", url: "https://github.com/alemiranda1105")))
             .previewDevice(PreviewDevice(rawValue: "iPhone 14"))
             .previewDisplayName("iPhone 14")
         
